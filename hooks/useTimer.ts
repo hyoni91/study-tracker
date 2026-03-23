1a323881ba68dd766353cc6e52a7461159e92c0a
hyoni91 <hyoni.green@gmail.com>
2026-03-23 09:21:38 +0900

    useTimer 선언

---
+++ hooks/useTimer.ts
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+{/** 최종반환값 */}
+{ /* *
+  isRunning,
+  elapsedTime,
+  start,
+  end,
+  reset
+*/}
+
+
+function useTimer (){
+
+
+const [startTime, setStartTime] = useState();
+const [isRunning, setIsRunning] = useState(false);
+
+const [endTime, setEndTime] = useState();
+const [duration, setDuration] = useState();
+
+{/** start */}
+// startTime, isRunning 
+
+{/** end */}
+
+// endTime, duration, return, StudyRecord 저장, isRunning 
+
+{/** Reset */}
+
+//초기화
+
+{/** update */}
+
+//1초마다 elapsedTime 업데이트
+
+
+}
+
