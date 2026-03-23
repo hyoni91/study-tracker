07983d8251d26dc1ddf5435accc74e170b337ff8
hyoni91 <hyoni.green@gmail.com>
2026-03-24 08:59:08 +0900

    useTimers v1

--- hooks/useTimer.ts
+++ hooks/useTimer.ts
@@ -1,70 +1,47 @@
-import { useRef, useState } from "react";
+"use client"
 
-{/** 최종반환값 */}
-{ /* *
-  isRunning,
-  elapsedTime,
-  start,
-  end,
-  reset
-*/}
-
+import { useState } from "react";
 
 function useTimer (){
 
+    const [startTime, setStartTime] = useState<number | null>(null);
+    const [isRunning, setIsRunning] = useState(false);
 
-const [startTime, setStartTime] = useState<number | null>();
-const [isRunning, setIsRunning] = useState(false);
-const [endTime, setEndTime] = useState<number | null>();
-const [duration, setDuration] = useState(0);
+    const start = () =>{
 
-const start = () =>{
+        //오늘 날짜가 있다면 더하기
+        if(isRunning) return;
 
-    //오늘 날짜가 있다면 더하기
-    if(isRunning) return;
+        const now = Date.now();
+        setStartTime(now)
+        setIsRunning(true)
 
-    const now = Date.now();
-    setStartTime(now)
-    setIsRunning(true)
+     }
 
+    const end = () =>{
+
+        //오늘 날짜가 있다면 더하기?
+        if(!isRunning) return;
+        if(startTime == null) return;
+
+        const now = Date.now()
+        const duration = now - startTime;
+        setIsRunning(false);
+
+        return{
+            startTime,
+            endTime: now,
+            duration,
+
+        }
     }
 
-const End = () =>{
-
-    //오늘 날짜가 있다면 더하기?
-    if(!isRunning) return;
-    if(startTime == null) return;
-
-    const now = Date.now()
-    const elapsed = now - startTime;
-    setDuration(elapsed);
-    setEndTime(now);
-    setIsRunning(false);
-
-}
-
-return{
-    startTime, endTime, duration
-}
+return {
+    start,
+    end,
+    isRunning,
+    startTime,
+  };
 
 
 }
-
-
-{/** end */}
-
-
-
-// endTime, duration, return, StudyRecord 저장, isRunning 
-
-{/** Reset */}
-
-//초기화
-
-{/** update */}
-
-//1초마다 elapsedTime 업데이트
-
-
-
-
