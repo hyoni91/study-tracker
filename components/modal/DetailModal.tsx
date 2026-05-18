9e5c4be89a3d195b2a777641651e548a4b803068
hyoni91 <hyoni.green@gmail.com>
2026-05-18 09:10:04 +0900

    feat:DetailModal버튼,언더라인추가

--- components/modal/DetailModal.tsx
+++ components/modal/DetailModal.tsx
@@ -10,17 +10,28 @@ export default function DetailModal({date}:{date:string}){
     const records = studyData[date] || [];
 
     return(
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-            <div className="bg-white p-4 rounded">
+        <div className="fixed inset-0  flex items-center justify-center">
+            <div className="bg-zinc-50 border border-gray-300 p-4 rounded  w-1/3 m-auto h-2/3 overflow-y-auto"
+                onClick={(e) => e.stopPropagation()}
+            >
                 <h2 className="text-xl font-bold mb-4">Study Details for {date}</h2>
                 <div>
                     {records.length === 0 ? (
                         <p>No study records for {date}</p>
                     ) : (
                         records.map((record, index) => (
-                            <div key={index} className="border-b py-2">
-                                <h3 className="font-semibold">{record.title}</h3>
-                                <p>Duration: {formatDurationShort(record.duration)}</p>
+                            <div key={index} className="py-2">
+                                <h3 className="font-semibold">TITLE : {record.title}</h3>
+                                <p className="text-gray-600 border-b mb-2">Duration : {formatDurationShort(record.duration)}</p>
+                                <textarea className="w-full h-20 p-2 border rounded" readOnly value={record.description} />
+                                <div>
+                                    <button type="button" className="bg-blue-500 text-white px-4 py-2 rounded">
+                                        Edit
+                                    </button>
+                                    <button type="button" className="bg-red-500 text-white px-4 py-2 rounded ml-2">
+                                        Delete
+                                    </button>
+                                </div>
                             </div>
                         ))
                     )}
